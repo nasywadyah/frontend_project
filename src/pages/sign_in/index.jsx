fad180172212d620a8c384ca51b3fef2276f7f28
Chaesya <alyadestriandy@gmail.com>
2025-03-16 11:20:28 +0700

    fix card layout

--- src/pages/sign_in/index.jsx
+++ src/pages/sign_in/index.jsx
@@ -33,13 +33,10 @@ const LoginForm = () => {
     setErrorMessage("");
 
     try {
-      const response = await axios.post(
-        "http://localhost:8000/api/auth/login",
-        {
-          email: formData.email,
-          password: formData.password,
-        }
-      );
+      const response = await axios.post("http://localhost:8000/api/auth/login", {
+        email: formData.email,
+        password: formData.password,
+      });
 
       localStorage.setItem("token", response.data.token);
 
@@ -58,66 +55,36 @@ const LoginForm = () => {
   };
 
   return (
-    <div className="flex justify-center w-full">
+    <div className="flex items-center justify-center w-full mt-20">
       <div className=" bg-white p-8 rounded-lg shadow-lg w-96">
-        <h2 className="text-xl font-semibold text-center">
-          Welcome To Finovate
-        </h2>
-        <h4 className="text-gray-500 text-center mb-6 text-sm mt-2 text-ms">
-          Welcome back! Please enter your details
-        </h4>
+        <h2 className="text-xl font-semibold text-center">Welcome To Finovate</h2>
+        <h4 className="text-gray-500 text-center mb-6 text-sm mt-2 text-ms">Welcome back! Please enter your details</h4>
 
         {/* Form */}
         <form onSubmit={handleSubmit}>
           {/* Email Input */}
           <div className="mb-3">
-            <label className=" text-left block text-gray-700 text-sm font-medium mb-2">
-              Email:
-            </label>
-            <input
-              type="email"
-              name="email"
-              value={formData.email}
-              onChange={handleChange}
-              placeholder="Masukkan email"
-              className="w-full mt-1 px-3 py-2 border rounded-lg text-sm"
-              required
-            />
+            <label className=" text-left block text-gray-700 text-sm font-medium mb-2">Email:</label>
+            <input type="email" name="email" value={formData.email} onChange={handleChange} placeholder="Masukkan email" className="w-full mt-1 px-3 py-2 border rounded-lg text-sm" required />
           </div>
 
           {/* Password Input */}
           <div className="mb-3 relative">
             <div className="flex justify-between">
-              <label className=" text-left block text-gray-700 font-medium text-sm mb-2">
-                Password:
-              </label>
+              <label className=" text-left block text-gray-700 font-medium text-sm mb-2">Password:</label>
               <div className="text-xs opacity-60" onClick={click_pass}>
                 {statusHide ? (
-                  <div
-                    className="cursor-pointer"
-                    onClick={() => setpasswordVisible(true)}
-                  >
+                  <div className="cursor-pointer" onClick={() => setpasswordVisible(true)}>
                     <i className="fa-solid fa-eye text-xs"></i> Unhide
                   </div>
                 ) : (
-                  <div
-                    className="cursor-pointer"
-                    onClick={() => setpasswordVisible(false)}
-                  >
+                  <div className="cursor-pointer" onClick={() => setpasswordVisible(false)}>
                     <i className="fa-solid fa-eye-slash text-xs"></i> Hide
                   </div>
                 )}
               </div>
             </div>
-            <input
-              type={passwordVisible ? "text" : "password"}
-              name="password"
-              value={formData.password}
-              onChange={handleChange}
-              placeholder="Masukkan password"
-              className="w-full mt-1 px-3 py-2 border rounded-lg text-ms"
-              required
-            />
+            <input type={passwordVisible ? "text" : "password"} name="password" value={formData.password} onChange={handleChange} placeholder="Masukkan password" className="w-full mt-1 px-3 py-2 border rounded-lg text-ms" required />
           </div>
 
           {/* Lupa Password */}
@@ -125,10 +92,7 @@ const LoginForm = () => {
             <Link to="/request-password-reset">Forgot password?</Link>
           </div>
           {/* Tombol Login */}
-          <button
-            type="submit"
-            className="w-full bg-blue-500 text-white py-2 rounded-lg hover:bg-blue-600"
-          >
+          <button type="submit" className="w-full bg-blue-500 text-white py-2 rounded-lg hover:bg-blue-600">
             {isLoading ? "Loading..." : "Login"}
           </button>
           {errorMessage && <p className="text-red-500 mt-2">{errorMessage}</p>}
@@ -143,15 +107,8 @@ const LoginForm = () => {
 
         {/* Tombol Login dengan Google */}
         <div className="flex rounded-lg justify-center">
-          <button
-            onClick={handleGoogleLogin}
-            className="flex items-center gap-2 px-6 py-2 border border-gray-300 rounded-full shadow-sm hover:bg-gray-100"
-          >
-            <img
-              src="https://www.gstatic.com/firebasejs/ui/2.0.0/images/auth/google.svg"
-              alt="Google Logo"
-              className="w-5 h-5 mr-2"
-            />
+          <button onClick={handleGoogleLogin} className="flex items-center gap-2 px-6 py-2 border border-gray-300 rounded-full shadow-sm hover:bg-gray-100">
+            <img src="https://www.gstatic.com/firebasejs/ui/2.0.0/images/auth/google.svg" alt="Google Logo" className="w-5 h-5 mr-2" />
             Continue with Google
           </button>
         </div>
