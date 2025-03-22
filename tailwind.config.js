ad6bd6ab111d80189b40fe4b8146a0f682b66d47
Arfa Banyu Santoro <banyuarfa1122@gmail.com>
2025-03-22 15:37:34 +0700

    fix(export): change module.exports to export default

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,27 +1,27 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./index.html", "./src/**/*.{html,js,jsx}"],
+export default {
+  content: ['./index.html', './src/**/*.{html,js,jsx}'],
   theme: {
     fontFamily: {
-      sans: "Mulish",
+      sans: 'Mulish',
     },
     screens: {
-      'sm': {'min': '640px', 'max': '767px'},
-      'md': {'min': '768px', 'max': '1023px'},
-      'lg': {'min': '1024px', 'max': '1279px'},
-      'xl': {'min': '1280px', 'max': '1535px'},
-      '2xl': {'min': '1536px'},
+      sm: { min: '640px', max: '767px' },
+      md: { min: '768px', max: '1023px' },
+      lg: { min: '1024px', max: '1279px' },
+      xl: { min: '1280px', max: '1535px' },
+      '2xl': { min: '1536px' },
     },
   },
-  plugins: [require("daisyui")],
+  plugins: [require('daisyui')],
   daisyui: {
     themes: false, // true: all themes | false: only light + dark | array: specific themes like this ["light", "dark", "cupcake"]
-    darkTheme: "light", // name of one of the included themes for dark mode
+    darkTheme: 'light', // name of one of the included themes for dark mode
     base: true, // applies background color and foreground color for root element by default
     styled: true, // include daisyUI colors and design decisions for all components
     utils: true, // adds responsive and modifier utility classes
     rtl: false, // rotate style direction from left-to-right to right-to-left. You also need to add dir="rtl" to your html tag and install `tailwindcss-flip` plugin for Tailwind CSS.
-    prefix: "", // prefix for daisyUI classnames (components, modifiers and responsive class names. Not colors)
+    prefix: '', // prefix for daisyUI classnames (components, modifiers and responsive class names. Not colors)
     logs: true, // Shows info about daisyUI version and used config in the console when building your CSS
   },
 };
